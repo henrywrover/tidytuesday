0ea7341d3f900558665583d6b9333ed471a8d706
Henry Wakefield <70211545+henrywrover@users.noreply.github.com>
2020-09-01 13:18:24 +0100

    Add files via upload

--- R/2020_09_01 - Crops.R
+++ R/2020_09_01 - Crops.R
@@ -1,4 +1,3 @@
-library(patchwork)
 library(tidyverse)
 library(RColorBrewer)
 library(extrafont)
@@ -6,10 +5,7 @@ theme_set(theme_minimal())
 options(scipen = 999)
 
 key_crop_yields <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-01/key_crop_yields.csv')
-fertilizer <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-01/cereal_crop_yield_vs_fertilizer_application.csv')
 tractors <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-01/cereal_yields_vs_tractor_inputs_in_agriculture.csv')
-land_use <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-01/land_use_vs_yield_change_in_cereal_production.csv')
-arable_land <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-01/arable_land_pin.csv')
 
 long_crops <- key_crop_yields %>% 
   pivot_longer(cols = 4:last_col(),
